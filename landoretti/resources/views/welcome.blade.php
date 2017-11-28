6e02317f7434be46045935bf62da16b8ce5b2b8a
Alessandro Aussems <alessandrossms@gmail.com>
2017-11-28 21:09:36 +0100

    Homepage_CarouselAdded

--- landoretti/resources/views/welcome.blade.php
+++ landoretti/resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Home</h1>
+    <div id="carousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel" data-slide-to="0"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+            <li data-target="#carousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="https://www.hdwallpapers.in/walls/autumn_bench-HD.jpg" alt="Slide 1">
+            </div>
+            <div class="item">
+                <img src="https://www.hdwallpapers.in/walls/autumn_bench-HD.jpg" alt="Slide 2">
+            </div>
+            <div class="item">
+                <img src="https://www.hdwallpapers.in/walls/autumn_bench-HD.jpg" alt="Slide 3">
+            </div>
+        </div>
 @endsection
