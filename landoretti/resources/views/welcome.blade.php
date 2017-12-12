c52c1644bb9514d66affd9f5924a663e92bceba6
Alessandro Aussems <alessandrossms@gmail.com>
2017-12-12 17:06:46 +0100

    ChangeImagesInSlideShow

--- landoretti/resources/views/welcome.blade.php
+++ landoretti/resources/views/welcome.blade.php
@@ -9,13 +9,13 @@
         </ol>
         <div class="carousel-inner">
             <div class="item active">
-                <img src="https://www.hdwallpapers.in/walls/autumn_bench-HD.jpg" alt="Slide 1">
+                <img src="http://www.photorating.net/images/2017/02/26/Color-Art-Abstract-4K-Wallpaperf768b.jpg" alt="Slide 1">
             </div>
             <div class="item">
-                <img src="https://www.hdwallpapers.in/walls/autumn_bench-HD.jpg" alt="Slide 2">
+                <img src="http://papers.co/wallpaper/papers.co-vu33-paint-texture-art-pattern-35-3840x2160-4k-wallpaper.jpg" alt="Slide 2">
             </div>
             <div class="item">
-                <img src="https://www.hdwallpapers.in/walls/autumn_bench-HD.jpg" alt="Slide 3">
+                <img src="http://wallpapericon.com/wp-content/uploads/2017/09/Abstract-Art-HD-Wallpaper-2017.jpg" alt="Slide 3">
             </div>
         </div>
 @endsection
