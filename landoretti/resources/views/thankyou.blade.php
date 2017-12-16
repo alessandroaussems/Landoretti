648a5ee0d2fb64421d48e3c4a62cfb4cd0b643f6
Alessandro Aussems <alessandrossms@gmail.com>
2017-12-16 13:16:04 +0100

    TakeMeHomeAfterPurchase

--- landoretti/resources/views/thankyou.blade.php
+++ landoretti/resources/views/thankyou.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
 <h1>Thank you for your purchase! {{\Illuminate\Support\Facades\Auth::user()->name}}</h1>
+<p>You will be redirected soon...</p>
+    <script>
+        setTimeout(function () {
+            window.location.href = "/";
+        }, 2500);
+    </script>
 @endsection
