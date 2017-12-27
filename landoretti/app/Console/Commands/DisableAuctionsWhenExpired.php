02cd8890ad6fcc7149361b869583cd083ca8baab
Alessandro Aussems <alessandrossms@gmail.com>
2017-12-27 19:56:30 +0100

    AddMessageLogicToCronjobSoUserGetMessages

--- landoretti/app/Console/Commands/DisableAuctionsWhenExpired.php
+++ landoretti/app/Console/Commands/DisableAuctionsWhenExpired.php
@@ -2,9 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\User;
 use Illuminate\Console\Command;
 use App\Auction;
 use Carbon\Carbon;
+use App\Bidding;
+use App\Message;
 
 class DisableAuctionsWhenExpired extends Command
 {
@@ -46,6 +49,23 @@ class DisableAuctionsWhenExpired extends Command
             {
                 $value->isactive=0;
                 $value->save();
+                $biddings=Bidding::where("auctionid",$value->id)->orderBy('biddingprice', 'DESC')->get();
+                if($biddings != array())
+                {
+
+                    $userwithhighestbidding=User::where("id",$biddings[0]->userid)->get();
+                    $userfromauction=User::where("id",$value->userid)->get();
+                    $messagetoauctionowner = new Message();
+                    $messagetoauctionowner->message = "Een gebruiker heeft uw kunstwerk " . $value->title . " gekocht!";
+                    $messagetoauctionowner->userid=$userfromauction[0]->id;
+                    $messagetoauctionowner->save();
+
+                    $messagetohighestbidder= new Message();
+                    $messagetohighestbidder->message = "Proficiat uw heeft het kunstwerk ". $value->title . " gekocht!";
+                    $messagetohighestbidder->userid=$userwithhighestbidding[0]->id;
+                    $messagetohighestbidder->save();
+
+                }
             }
         }
     }
