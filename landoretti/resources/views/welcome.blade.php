0492e5ce7b1de401038651b73e1853d64de5669e
Alessandro Aussems <alessandrossms@gmail.com>
2018-01-02 17:08:15 +0100

    SlideShowOptimization

--- landoretti/resources/views/welcome.blade.php
+++ landoretti/resources/views/welcome.blade.php
@@ -4,19 +4,20 @@
     <h1 id="welcome">{{ __('messages.welcome') }}</h1>
     <div id="carousel" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
-            <li data-target="#carousel" data-slide-to="0"></li>
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
             <li data-target="#carousel" data-slide-to="1"></li>
             <li data-target="#carousel" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
+            <!-- IMAGES ARE OPTIMIZED FOR THE WEB! by: http://optimizilla.com/ -->
             <div class="item active">
-                <img src="http://papers.co/wallpaper/papers.co-vu33-paint-texture-art-pattern-35-3840x2160-4k-wallpaper.jpg" alt="Slide 1">
+                <img src="{{asset("img/slides/slide1.jpg")}}" alt="Slide 1">
             </div>
             <div class="item">
-                <img src="http://papers.co/wallpaper/papers.co-vu33-paint-texture-art-pattern-35-3840x2160-4k-wallpaper.jpg" alt="Slide 2">
+                <img src="{{asset("img/slides/slide2.jpg")}}" alt="Slide 2">
             </div>
             <div class="item">
-                <img src="http://wallpapericon.com/wp-content/uploads/2017/09/Abstract-Art-HD-Wallpaper-2017.jpg" alt="Slide 3">
+                <img src="{{asset("img/slides/slide3.jpg")}}" alt="Slide 3">
             </div>
         </div>
 @endsection
